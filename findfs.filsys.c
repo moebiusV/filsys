a975e54d40ade4d0cb2b809dd4d5a2749e8e71fc
David Walther <david@clearbrookdistillery.com>
2026-09-06 12:18:31 -0700

    filsys 1.8.0: PDP-7 word container codec + packing selection

--- findfs.filsys.c
+++ findfs.filsys.c
@@ -1,4 +1,4 @@
-/* filsys 1.7.0 - 2026-09-06 - Copyright (C) 2026 David Walther */
+/* filsys 1.8.0 - 2026-09-06 - Copyright (C) 2026 David Walther */
 /* SPDX-License-Identifier: ISC */
 /* findfs.filsys.c - locate filesystem superblocks on a raw disk image.
  *
