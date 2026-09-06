5ec3e0e8f773bfa54a8a431b05b92225e2e40df7
David Walther <david@clearbrookdistillery.com>
2026-09-06 11:41:39 -0700

    filsys 1.7.0: fold the block-map tree walk and check helpers

--- mkfs.filsys.c
+++ mkfs.filsys.c
@@ -1,4 +1,4 @@
-/* filsys 1.6.0 - 2026-09-06 - Copyright (C) 2026 David Walther */
+/* filsys 1.7.0 - 2026-09-06 - Copyright (C) 2026 David Walther */
 /* SPDX-License-Identifier: ISC */
 /* mkfs.filsys.c - create a Research Unix (PDP-7 through 32V) filesystem in a
  * disk image.
