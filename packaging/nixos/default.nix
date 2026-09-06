789ec693aa88c9551cc1cb861da1ca10014f925c
David Walther <david@clearbrookdistillery.com>
2026-09-05 22:23:04 -0700

    packaging: refresh checksums for the 2.0.2 tarball

--- packaging/nixos/default.nix
+++ packaging/nixos/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "filsys";
-  version = "2.0.1";
+  version = "2.0.2";
 
   src = fetchurl {
     url = "https://github.com/moebiusV/filsys/releases/download/v${version}/filsys-${version}.tar.gz";
