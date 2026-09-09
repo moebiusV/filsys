dd9a744ea671355c8528a1b026e1333c4d130dc1
David Walther <david@clearbrookdistillery.com>
2026-09-09 11:31:31 -0700

    Release 3.0.2: housekeeping version bump
    - configure.ac/NEWS 3.0.1 -> 3.0.2; NEWS notes the packaging coverage
      corrections and the README source-only note.
    - Bump packaging templates to 3.0.2; rename the Gentoo ebuild.

--- packaging/nixos/default.nix
+++ packaging/nixos/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "filsys";
-  version = "3.0.1";
+  version = "3.0.2";
 
   src = fetchurl {
     url = "https://github.com/moebiusV/filsys/releases/download/v${version}/filsys-${version}.tar.gz";
