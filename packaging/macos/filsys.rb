145cf12f5eb277c9be0d698ddd9960cb5a5a4002
David Walther <david@clearbrookdistillery.com>
2026-09-14 05:21:29 -0700

    packaging: pin the 4.1.2 tarball checksums
    SHA256 1cca6d5c8e44507bfd16a103456e55d34ac699cc1f2e7b3c7c286068d0d3e31d
    SHA512 96a418c2dc77fc17d2d50845ff06ff41aecf5fb74e0313c357549edc8156de35100c4a6e9a87a8e1c64be2a536a129e6d121ee5c15b9db7ece683016e0f04106
    BLAKE2s dd0b4fd9c75815482b5c9d5ba6b379d8242f7a505157add79bdf9d1d22b6355adef0293517b865d19a29203fd90d71706cba3337e1cc5890c7e4c22d1ff7dd24
    MD5 fe0acb0df06da1874023c51a21e06e2c
    size 361892

--- packaging/macos/filsys.rb
+++ packaging/macos/filsys.rb
@@ -8,7 +8,7 @@ class Filsys < Formula
   desc "FUSE driver for Research Unix (PDP-7 through V10, 32V, Coherent, Xenix, 2.9/2.11BSD, System III/V) filesystem images"
   homepage "https://github.com/moebiusV/filsys"
   url "https://github.com/moebiusV/filsys/releases/download/v4.1.2/filsys-4.1.2.tar.gz"
-  sha256 "0911a38beb9b13df6d4cbbcb99449b3e966e7e2c2ab8a4d25f95e43319677ab3"
+  sha256 "1cca6d5c8e44507bfd16a103456e55d34ac699cc1f2e7b3c7c286068d0d3e31d"
   # ISC (see COPYING).
   license "ISC"
 
